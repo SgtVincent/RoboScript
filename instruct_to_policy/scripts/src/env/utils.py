0ba695c63c2b4c9bfba3e97b4409b778542a4c29
Junting Chen <sgt79911030@gmail.com>
2023-08-15 18:23:00 +0800

    added workaround to publish models in moveit

--- instruct_to_policy/scripts/src/env/utils.py
+++ instruct_to_policy/scripts/src/env/utils.py
@@ -1,6 +1,9 @@
 from typing import List
 from geometry_msgs.msg import Pose, PoseStamped
-
+import xml 
+import xml.etree.ElementTree as ElementTree
+from moveit_commander import PlanningSceneInterface
+import rospy 
 
 ################## ROS utils ###################
 
@@ -21,3 +24,74 @@ def get_stamped_pose(position: List, orientation: List, frame_id="world") -> Pos
     pose_stamped.header.frame_id = frame_id
     pose_stamped.pose = get_pose_msg(position, orientation)
     return pose_stamped
+
+#################### Gazebo and MoveIt utils ####################
+
+def load_model_into_moveit(sdf_path, pose: Pose, scene: PlanningSceneInterface, model_name="model", link_name="link", ref_frame="world"):
+    # try:    
+    shape_idx = 0
+    # convert pose to pose stamped
+    pose_stamped = get_stamped_pose(
+        [pose.position.x, pose.position.y, pose.position.z], 
+        [pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w], 
+        ref_frame
+    )
+    # 
+    if model_name == "ground_plane":
+        scene.add_plane(f"{model_name}_{shape_idx}_plane", pose_stamped)
+        return 
+    
+    # parse model from sdf file 
+    # TODO: create class to maintain all loaded models in memory 
+    # with open(sdf_path, 'r') as f:
+    #     sdf_xml = f.read()
+    tree = ElementTree.parse(sdf_path)
+    root = tree.getroot()
+
+    # NOTE: Assume there is only one link for the object 
+    # Iterate over models and collision geometries
+    link = root.find(f".//link[@name='{link_name}']")
+    for collision in link.findall(".//collision"):
+        collision_geometry = collision.find(".//geometry")
+        
+        # Iterate over all shapes in the collision geometry
+        for shape in collision_geometry:
+            # Add collision object to moveit planning scene by shape type
+
+            if shape.tag == "box":
+                size = [float(value) for value in shape.find("size").text.split()]
+                pose = link.find(".//pose").text  # Adjust pose if needed
+                scene.add_box(f"{model_name}_{shape_idx}_box", pose_stamped, size)
+                shape_idx += 1
+
+            elif shape.tag in "sphere":
+                radius = float(shape.find("radius").text)
+                scene.add_sphere(f"{model_name}_{shape_idx}_sphere", pose_stamped, radius)
+                shape_idx += 1
+
+            elif shape.tag == "cylinder":
+                radius = float(shape.find("radius").text)
+                length = float(shape.find("length").text)
+                scene.add_cylinder(f"{model_name}_{shape_idx}_cylinder", pose_stamped, length, radius)
+                shape_idx += 1
+
+            elif shape.tag == "cone":
+                radius = float(shape.find("radius").text)
+                length = float(shape.find("length").text)
+                scene.add_cone(f"{model_name}_{shape_idx}_cone", pose_stamped, length, radius)
+                shape_idx += 1
+    
+            elif shape.tag == "plane":
+                normal = [float(value) for value in shape.find("normal").text.split()]
+                # size not meaningful for moveit
+                scene.add_plane(f"{model_name}_{shape_idx}_plane", pose_stamped, normal)
+                shape_idx += 1
+
+            elif shape.tag == "mesh":
+                # raise NotImplementedError("Meshes are not supported yet")
+                rospy.logwarn(f"Mesh loading not supported yet, coming soon...")
+            else:
+                rospy.logwarn(f"SolidPrimitive {shape.tag} type not supported")
+
+    # except Exception as e:
+    #     return e 

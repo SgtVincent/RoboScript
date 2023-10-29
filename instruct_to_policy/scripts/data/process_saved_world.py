4f3090ad8e91ea129a8a0d2c8ce010d5f2630384
Junting Chen <sgt79911030@gmail.com>
2023-10-29 21:42:39 +0800

    updated script to process gazebo saved world

--- instruct_to_policy/scripts/data/process_saved_world.py
+++ instruct_to_policy/scripts/data/process_saved_world.py
@@ -3,7 +3,9 @@ This script is used to process the raw world file saved from Gazebo.
 - Instead of saving whole model definition, only save model handles
 """
 
-import lxml.etree as ET
+# import lxml.etree as ET
+import xml.etree.ElementTree as ET
+import xml.dom.minidom as minidom
 import numpy as np
 from argparse import ArgumentParser
 import re
@@ -16,6 +18,8 @@ def parse_args():
     parser.add_argument('--input_world', type=str, required=True)
     parser.add_argument('--output_world', type=str, required=True)
     parser.add_argument('--base_world', type=str, default=default_base_world)
+    parser.add_argument('--exclude_models', type=str, nargs='*', 
+                        default=["sun", "table", "ground_plane", "panda", "triple_camera_set"])
     return parser.parse_args()
 
 if __name__ == '__main__':
@@ -24,66 +28,48 @@ if __name__ == '__main__':
     tree = ET.parse(args.input_world)
     root = tree.getroot()  
 
+    # Parse base XML file and write with indentations
+    out_tree = ET.parse(args.base_world)
+    out_root = out_tree.getroot()
+    out_world_tag = out_root.find('world')
+
     # Find all 'model' tags under 'world/state' and get name, pose, scales
     models = []
     poses = {}
     scales = {}
-    for model in root.find('world/state').iter('model'):
+    for model in root.findall('world/state/model'):
         name = model.attrib['name']
+        if name in args.exclude_models:
+            continue
+        
         pose = model.find('pose').text
         scale = model.find('scale').text
         models.append(name)
         poses[name] = pose
         scales[name] = scale
 
-    # Find all model URIs    
-    uris = {}    
-    for model in root.find('world').iter('model'):
+    # Copy model tags from input XML to output XML and 
+    # add/replace pose, scale tags      
+    for model in root.findall('world/model'):
         name = model.attrib['name']
-        uri_tag = model.find('link/collision/geometry/mesh/uri')
-        # all custom models have a link with name "link"
-        # if not found, then it should be a environment model, skip it 
-        if uri_tag is not None:
-            uri = uri_tag.text
-            # get root folder of uri (e.g. 'model://013_apple/google_16k/textured.obj' to 'model://013_apple')
-            # use regex
-
-            # first pattern
-            pattern = r"^(model://[^/]+)/"
-            match = re.search(pattern, uri)
-            if match is not None:
-                root_uri = match.group(1)
-                uris[name] = root_uri
-            else:
-                # try second pattern
-                pattern = r"/models/([^/]+)/"
-                match = re.search(pattern, uri)
-                if match is not None:
-                    model_id = match.group(1)
-                    root_uri = "model://" + model_id
-                    uris[name] = root_uri
-                else:
-                    print("Error: cannot find root uri for uri {}".format(uri))
-  
-    # Parse base XML file and write with indentations
-    parser = ET.XMLParser(remove_blank_text=True)
-    out_tree = ET.parse(args.base_world, parser)
-    out_root = out_tree.getroot()
-    sdf_tag = out_root.find('world')
-
-    # Add include tags to base XML
-    # the uri, name, pose and scale should be added as subtags of include tag 
-    for name in models:
-        pose = poses[name]
-        scale = scales[name]
-        if name in uris:
-            # print("Adding model {} to world".format(name))
-            uri = uris[name]
-            include_tag = ET.SubElement(sdf_tag, 'include')
-            ET.SubElement(include_tag, 'name').text = name
-            ET.SubElement(include_tag, 'uri').text = uri
-            ET.SubElement(include_tag, 'pose').text = pose
-            ET.SubElement(include_tag, 'scale').text = scale     
-
-    # Write output XML        
-    out_tree.write(args.output_world, pretty_print=True)
+        if name in args.exclude_models:
+            continue
+        
+        # Copy model tag
+        out_world_tag.append(model)
+        # replace pose tag if exists, otherwise add pose tag
+        pose_tag = model.find('pose')
+        if pose_tag is None:
+            pose_tag = ET.SubElement(model, 'pose')
+        pose_tag.text = poses[name]
+        # replace scale tag if exists, otherwise add scale tag
+        scale_tag = model.find('scale')
+        if scale_tag is None:
+            scale_tag = ET.SubElement(model, 'scale')
+        scale_tag.text = scales[name]
+        
+    # Write output XML with formant     
+    xmlstr = minidom.parseString(ET.tostring(out_root)).toprettyxml(indent="  ")
+    xmlstr = os.linesep.join([s for s in xmlstr.splitlines() if s.strip()])
+    with open(args.output_world, "w") as f:
+        f.write(xmlstr)

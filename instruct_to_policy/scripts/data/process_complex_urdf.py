63eda36a1699e793bbb06f29040629421187a4b4
SgtVincent <sgt79911030@gmail.com>
2023-12-11 22:46:27 +0800

    add script to process complex urdf (maniskill)

--- instruct_to_policy/scripts/data/process_complex_urdf.py
+++ instruct_to_policy/scripts/data/process_complex_urdf.py
@@ -35,7 +35,7 @@ def as_mesh(scene_or_mesh):
         mesh = scene_or_mesh
     return mesh
 
-def parse_mesh_path(mesh_file: str):
+def parse_mesh_path(mesh_file: str, mesh_dir):
     file_tag = "file://"
     model_tag = "model://"
     mesh_path = mesh_file
@@ -54,10 +54,12 @@ def parse_mesh_path(mesh_file: str):
                 return mesh_path
         # if not found, raise error
         raise ValueError(f"Mesh file {mesh_file} not found in GAZEBO_MODEL_PATH:\n{gazebo_model_path}")
-    
+    else: # relative path
+        mesh_path = os.path.join(mesh_dir, mesh_file)
+        
     return mesh_path
 
-def compute_intertial_link(link_element: ET.Element, use_visual_mesh=False, density=10.0, min_mass=0.1):
+def compute_intertial_link(link_element: ET.Element, mesh_dir, use_visual_mesh=False, density=10.0, min_mass=0.1):
 
     total_mass = 0.0
     total_inertia = np.zeros((3, 3))
@@ -74,7 +76,7 @@ def compute_intertial_link(link_element: ET.Element, use_visual_mesh=False, dens
             # sdf format
             if mesh_file == "":
                 mesh_file = mesh_element.find("uri").text
-            mesh_path = parse_mesh_path(mesh_file)
+            mesh_path = parse_mesh_path(mesh_file, mesh_dir)
             mesh = trimesh.load_mesh(mesh_path, force="mesh", skip_texture=True)
             mesh_list.append(mesh)
 
@@ -92,31 +94,21 @@ def compute_intertial_link(link_element: ET.Element, use_visual_mesh=False, dens
         
     return combined_origin, total_mass, total_inertia, combined_mesh
 
-
-def add_intertial_urdf(urdf_path, output_urdf_path, density, use_visual_mesh=False, debug=False):
-    """
-    Add intertial tag to each link of the urdf file.
-    Example: 
-    <inertial>
-        <origin xyz="0 0 0.5" rpy="0 0 0"/>
-        <mass value="1"/>
-        <inertia ixx="100"  ixy="0"  ixz="0" iyy="100" iyz="0" izz="100" />
-    </inertial>
-    """
-    parser = ET.XMLParser(remove_blank_text=True)
-    tree = ET.parse(urdf_path, parser)
-    root = tree.getroot()
-    # add inertial tag to each link
-    for link in root.findall('link'):
-        # if link has inertial tag, skip
+def add_interial_tag_to_link(link: ET.Element, mesh_dir, use_visual_mesh=False, density=10.0, min_mass=0.1, overwrite=False, export_combined_mesh=False):
+    # if link has inertial tag, skip
         if link.find('inertial') is not None:
-            continue
+            if overwrite:
+                # remove the current inertial tag
+                inertial = link.find('inertial')
+                link.remove(inertial)
+            else:
+                return 
         # if link has no visual or collision tag, skip
         if link.find('visual') is None and link.find('collision') is None:
-            continue
+            return 
         # calculate mass and inertia
-        origin, mass, inertia, mesh = compute_intertial_link(link, use_visual_mesh, density=density)
-        if debug:
+        origin, mass, inertia, mesh = compute_intertial_link(link, mesh_dir=mesh_dir, use_visual_mesh=use_visual_mesh, density=density)
+        if export_combined_mesh:
             # visualize combined mesh
             link_name = link.attrib['name']
             mesh.export(f"{link_name}.obj")
@@ -134,18 +126,72 @@ def add_intertial_urdf(urdf_path, output_urdf_path, density, use_visual_mesh=Fal
         inertia_tag.attrib['iyy'] = str(inertia[1, 1])
         inertia_tag.attrib['iyz'] = str(inertia[1, 2])
         inertia_tag.attrib['izz'] = str(inertia[2, 2])
+        
+
+def add_limit_tag_to_joint(joint: ET.Element, lower_limit=0.0, upper_limit=0.3, effort=1000.0, velocity=0.5, overwrite=False):
+    """
+    Add limit tag to joint.
+    Example:
+    <joint name="joint_0" type="prismatic">
+		<origin xyz="0 0 0" />
+		<axis xyz="0 0 1" />
+		<child link="link_0" />
+		<parent link="link_3" />
+		<limit ... />
+	</joint>
+    """
+    # if joint tag has limit tag, skip or overwrite
+    if joint.find('limit') is not None:
+        if overwrite:
+            # remove the current limit tag
+            limit = joint.find('limit')
+            joint.remove(limit)
+        else:
+            return
     
+    # add tag to joint
+    limit = ET.SubElement(joint, 'limit')
+    limit.attrib['lower'] = str(lower_limit)
+    limit.attrib['upper'] = str(upper_limit)
+    limit.attrib['effort'] = str(effort)
+    limit.attrib['velocity'] = str(velocity)
+    
+def process_urdf(urdf_path, output_urdf_path, density, use_visual_mesh=False, debug=False):
+    """
+    Add intertial tag to each link of the urdf file.
+    Example: 
+    <inertial>
+        <origin xyz="0 0 0.5" rpy="0 0 0"/>
+        <mass value="1"/>
+        <inertia ixx="100"  ixy="0"  ixz="0" iyy="100" iyz="0" izz="100" />
+    </inertial>
+    """
+    parser = ET.XMLParser(remove_blank_text=True)
+    tree = ET.parse(urdf_path, parser)
+    dir_path = os.path.dirname(urdf_path)
+    root = tree.getroot()
+    # add inertial tag to each link
+    for link in root.findall('link'):
+
+        # add inertial tag
+        add_interial_tag_to_link(link, mesh_dir=dir_path, use_visual_mesh=use_visual_mesh, density=density, overwrite=False, export_combined_mesh=debug)
+
+    for joint in root.findall('joint'):
+        # add limit tag to each joint
+        add_limit_tag_to_joint(joint, overwrite=True)
+        
     # write to file with indentations
     tree.write(output_urdf_path, pretty_print=True)  
     print(f"URDF with intertial tags written to {output_urdf_path}.")
 
-
 if __name__ == '__main__':
 
-    urdf_path = "/home/junting/Downloads/Compressed/pot/100021/mobility_relabel_gapartnet.urdf"
-    output_urdf_path = "/home/junting/Downloads/Compressed/pot/100021/mobility_relabel_gapartnet_inertial.urdf"
+    # urdf_path = "/home/junting/Downloads/Compressed/pot/100021/mobility_relabel_gapartnet.urdf"
+    # output_urdf_path = "/home/junting/Downloads/Compressed/pot/100021/mobility_relabel_gapartnet_inertial.urdf"
+    urdf_path = "/home/junting/Downloads/drawer_45290-20231211T112144Z-001/drawer_45290/mobility_relabel_gapartnet_coacd.urdf"
+    output_urdf_path = "/home/junting/Downloads/drawer_45290-20231211T112144Z-001/drawer_45290/mobility_relabel_gapartnet_coacd_inertial.urdf"
     density = 10.0
     # os change directory to urdf directory
     os.chdir(os.path.dirname(urdf_path))
-    add_intertial_urdf(urdf_path, output_urdf_path, density, debug=False)
+    process_urdf(urdf_path, output_urdf_path, density, debug=False)
     
